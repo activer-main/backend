985bbb6a9770e7aef70dbb33f11d08c117ee71b0
DACHXY <danny10132024@gmail.com>
2023-03-13 22:09:41 +0800

    init Project

---
+++ Models/DBEntity/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace ActiverWebAPI.Models.DBEntity;
+
+public class BaseEntity
+{
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+}
