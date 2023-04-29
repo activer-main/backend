e9a11c299cc64cad43fa511b66c31296830f51f5
DACHXY <danny10132024@gmail.com>
2023-04-29 17:39:51 +0800

    解決 activityController 中的引用模糊

--- Controllers/ActivityController.cs
+++ Controllers/ActivityController.cs
@@ -118,7 +118,7 @@ public class ActivityController : BaseController
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<ActionResult<List<ActivityDTO>>> PostActivities(List<ActivityPostDTO> activityPostDTOs)
     {
-        var activities = _mapper.Map<List<Activity>>(activityPostDTOs);
+        var activities = _mapper.Map<List<Models.DBEntity.Activity>>(activityPostDTOs);
 
         foreach (var activity in activities)
         {
