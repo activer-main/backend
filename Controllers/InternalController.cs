170029ff07c98f12b09147df9558d2175b75f146
DACHXY <danny10132024@gmail.com>
2023-05-07 17:24:05 +0800

    刪除不必要的引用

--- Controllers/InternalController.cs
+++ Controllers/InternalController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using ActiverWebAPI.Models.DBEntity;
-using Azure;
 
 namespace ActiverWebAPI.Controllers;
 
